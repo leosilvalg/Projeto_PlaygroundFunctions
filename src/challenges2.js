7f0c1b29f8fda88409a3d5c7288ab1ecd1d6f7d8
Leonardo Gonçalves <leonardorc.geo@gmail.com>
2021-12-02 21:11:34 -0300

    Resoluçao do 12 e do 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,18 +4,42 @@ function techList() {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  var numero = array;
+  
+
+    if (array.length === 11) {
+      var resultado = array.replace(/(\d{ 2 })(\d{ 5 })(\d{ 4 })/, "($1)-$2-$3");
+    } else 
+       return "Array com tamanho incorreto."
+    
+    return resultado
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(LineA, LineB, LineC) {
+  let resultado = false
+
+  if (LineA < (LineB + LineC) && LineA > Math.abs(LineB - LineC) || LineB < (LineA + LineC) && LineB > Math.abs(LineA - LineC) || LineC < (LineA + LineB) && LineC > Math.abs(LineA - LineB)) {
+     resultado = true;
+  }
+  return resultado
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(string) {
+   let copos = 0;
+   var numero = string.match(/\d+/g).map(Number);
+
+   for(let i of numero){
+     copos += i
+   }
+
+   if(copos === 1){
+     return copos + ' ' + 'copo de água'
+   }
+
+  return copos + ' ' + 'copos de água'
 }
 
 module.exports = {
